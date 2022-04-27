fe5f9fa3fcff9266a6a7fc94572b37f3b96d0eb9
McKay Kleinman <vicrumkleinman@yahoo.com>
2022-04-27 14:25:16 -0600

    typo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const app = espress();
+const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
